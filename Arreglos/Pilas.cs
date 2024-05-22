0822ff96936ebcf7f86c3ad2cd6dc7b6ef7b7003
Jose Javier Mendoza Villalpando <Jose_Mendoza23@unikino.edu.mx>
2024-05-22 08:01:28 -0700

    Agregamos Referencia en app  desde arreglos y creamos la clase pilas

---
+++ Arreglos/Pilas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    public class Pilas
+    {
+        private string[] _arreglo;
+        private int _actual;
+        public Pilas(int tamanio)
+        {
+            _arreglo = new string[tamanio];
+            _actual = 0;
+        }
+
+        private bool EstaVacio() 
+        {
+            return ( _actual == 0 );
+        }
+    }
+}
