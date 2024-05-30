16e7cf093565810cedaa4fc3697ea1fc9eb94c86
Jose Javier Mendoza Villalpando <Jose_Mendoza23@unikino.edu.mx>
2024-05-30 07:34:55 -0700

    Terminando clase pilas del proyecto listas.

--- App/Program.cs
+++ App/Program.cs
@@ -1,38 +1,38 @@
-﻿Arreglos.Pilas pilas = new Arreglos.Pilas(10);
-Arreglos.Colas cola = new Arreglos.Colas(10);
+﻿Listas.Pilas pilas = new Listas.Pilas();
+//Arreglos.Colas cola = new Arreglos.Colas(10);
 
-Console.WriteLine(cola.ObtenerDatos());
-Console.WriteLine("-------");
-cola.Agregar("A");
-cola.Agregar("B");
-cola.Agregar("C");
-cola.Agregar("D");
-cola.Agregar("E");
-cola.Agregar("F");
-Console.WriteLine(cola.ObtenerDatos());
-Console.WriteLine("-------");
-
-cola.Eliminar();
-Console.WriteLine(cola.ObtenerDatos());
-Console.WriteLine("-------");
-cola.Eliminar();
-Console.WriteLine(cola.ObtenerDatos());
-Console.WriteLine("-------");
-
-//Console.WriteLine(value: pilas.ObtenerDatos());
+//Console.WriteLine(cola.ObtenerDatos());
 //Console.WriteLine("-------");
-//pilas.Agregar("A");
-//pilas.Agregar("B");
-//pilas.Agregar("C");
-//pilas.Agregar("D");
-//pilas.Agregar("E");
-//pilas.Agregar("F");
-//Console.WriteLine(value: pilas.ObtenerDatos());
+//cola.Agregar("A");
+//cola.Agregar("B");
+//cola.Agregar("C");
+//cola.Agregar("D");
+//cola.Agregar("E");
+//cola.Agregar("F");
+//Console.WriteLine(cola.ObtenerDatos());
 //Console.WriteLine("-------");
 
-//pilas.Eliminar();
-//Console.WriteLine(value: pilas.ObtenerDatos());
+//cola.Eliminar();
+//Console.WriteLine(cola.ObtenerDatos());
 //Console.WriteLine("-------");
-//pilas.Eliminar();
-//Console.WriteLine(value: pilas.ObtenerDatos());
+//cola.Eliminar();
+//Console.WriteLine(cola.ObtenerDatos());
 //Console.WriteLine("-------");
+
+Console.WriteLine(value: pilas.ObtenerDatos());
+Console.WriteLine("-------");
+pilas.Agregar("A");
+pilas.Agregar("B");
+pilas.Agregar("C");
+pilas.Agregar("D");
+pilas.Agregar("E");
+pilas.Agregar("F");
+Console.WriteLine(value: pilas.ObtenerDatos());
+Console.WriteLine("-------");
+
+pilas.Eliminar();
+Console.WriteLine(value: pilas.ObtenerDatos());
+Console.WriteLine("-------");
+pilas.Eliminar();
+Console.WriteLine(value: pilas.ObtenerDatos());
+Console.WriteLine("-------");
--- Listas/Pilas.cs
+++ Listas/Pilas.cs
@@ -1,4 +1,7 @@
-﻿namespace Listas
+﻿
+using System.Text;
+
+namespace Listas
 {
     public class Pilas
     {
@@ -10,6 +13,53 @@
         public void Agregar(string dato)
         {
             _list.Add(dato);
+        }
+        private bool ValidaVacio()
+        {
+            return _list.Count == 0;
+        }
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("Lista vacio");
+            }
+            _list.RemoveAt(_list.Count - 1);
+        }
+        public string Imprimir()
+        {
+            string dato = string.Empty;
+
+            if (ValidaVacio())
+            {
+                return "Lista vacia";
+            }
+            {
+                int Lstcount = _list.Count;
+                for (int i = 0; i < Lstcount; i++)
+                {
+                    if (i > 0)
+                    {
+                        dato += "\n";
+                    }
+                    dato += $"[{i}] - {_list[i]}";
+                }
+                return dato;
+            }
+        }
+
+        public string ObtenerDatos()
+        {
+            StringBuilder datos = new StringBuilder();
+            foreach (string dato in _list)
+            {
+                if (!string.IsNullOrWhiteSpace(dato))
+                {
+                    datos.Append(dato);
+                    datos.Append(Environment.NewLine);
+                }
+            }
+            return datos.ToString();
         }
     }
 }
